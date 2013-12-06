bea06abc4684176c7d1b03114992301779ef72ee
Rafael Lopes <dev@rafalop.es>
2013-12-06 19:29:18 -0200

    Putting to run forever

--- __init__.py
+++ __init__.py
@@ -1,21 +1,25 @@
 from zabbixdata import *
 from geckoboard import *
 import configs
+from time import sleep
 
+while True:
 
-gb = Geckoboard()
+    gb = Geckoboard()
 
-for piece in configs.pieces:
-    kind = piece['kind']
-    if kind == 'Line':
-        history = ZabbixData().gethistory(piece)
-        gb.line(history,piece)
-        gb.push(piece)
-    if kind == 'Monitoring':
-        webmondata = ZabbixData().gethistory(piece)
-        gb.monitoring(webmondata,piece)
-        gb.push(piece)
-    if kind == "Triggers":
-        triggers = ZabbixData().getunacktriggers(piece)
-        gb.triggerlist(triggers,piece)
-        gb.push(piece)
+    for piece in configs.pieces:
+        kind = piece['kind']
+        if kind == 'Item':
+            history = ZabbixData().gethistory(piece)
+            gb.line(history,piece)
+            gb.push(piece)
+        if kind == 'Monitoring':
+            webmondata = ZabbixData().gethistory(piece)
+            gb.monitoring(webmondata,piece)
+            gb.push(piece)
+        if kind == "Triggers":
+            triggers = ZabbixData().getunacktriggers()
+            gb.triggerlist(triggers,piece)
+            gb.push(piece)
+
+    sleep(60)
